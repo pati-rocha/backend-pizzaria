5e64321c716a2ee8d8002b1fe10a3db3a0eec195
Pati Rocha <patirocha.dev@gmail.com>
2023-04-08 22:17:23 -0300

    feat: update order status

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ app.get('/solicitations/:id', (request, response) => {
     response.json(solicitation)
 })
 
+//
+
 //cadastrar pedido
 app.post('/solicitations', (request, response) => {
 
@@ -81,11 +83,30 @@ app.post('/solicitations', (request, response) => {
         payment_method,
         observations,
         pizzas,
-        order: "EM PRODUÇÃO"        
+        order: "EM PRODUÇÃO"   
     }
     
     solicitations.push(solicitation)
     response.status(201).json(solicitation)
+})
+
+//atualizar status do pedido
+app.patch('/solicitations/:id/active', (request, response) => {
+
+    const solicitation = solicitations.find(solicitation => solicitation.id == request.params.id)
+    if(!solicitation) {
+        return response.status(404).json({error: "Desculpe, não encontramos seu pedido!"})
+    }
+    
+    const solicitationOrder = solicitations.map(solicitation => {
+        if (solicitation.id == request.params.id){
+            solicitation.order = "A CAMINHO"
+        }
+        return solicitation
+    })
+    //solicitations = [...solicitationOrder ]
+    response.json(solicitationOrder)   
+
 })
 
 app.listen(3333, () => {
